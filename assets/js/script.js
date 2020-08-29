e48a59093edafd9fcbe366fb6dcabb4870132373
Spiewak <bspiewak6@gmail.com>
2020-08-29 17:43:05 -0400

    updated highscore page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,22 @@
-var timerClock = document.getElementById('timer');
-//var quizSelection = document.getElementById('quiz');
-var startBtn = document.getElementById('start');
-var answerElement = document.getElementById('answer');
+var timerClock = document.getElementById("timer");
+var startBtn = document.getElementById("start");
+var answerEl = document.getElementById("answer");
+var questionEl = document.getElementById("question");
 var currentQuestion = 0;
 var scoreCounter = 0;
 
-// create variable with questions and answrs
-
+// create array for questions, choices and answers
 var questions = [
     {
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["<js>", "<javascript>", "<script>", "<scripting>"],
     answer: "<script>"
     },
-
-    
+ 
     {
     question: "Where is the correct place to insert a JavaScript?",
     choices: ["the <header> section", "both <header> and <body> section", "<body> section", "<footer> section"],
-    answer: "<body> section"
+    answer: "<body> section"   
     },
     
     {
@@ -30,7 +28,7 @@ var questions = [
     {
     question: "The condition in an if/else statement is enclosed with ______",
     choices: ["quotes", "parenthesis", "curly brackets", "square brackets"],
-    answer: "parenthesis" 
+    answer: "parenthesis"
     },
 
     {
@@ -40,23 +38,9 @@ var questions = [
     },
 ];
 
-function timer() {
-    var timeLeft = 75;
-
-    var interval = setInterval(function() {
-        if (timeLeft > 0) {
-            timerClock.textContent = timeLeft;
-            timeLeft--; 
-        } else {
-            clearInterval(interval);
-            // show highscores.html
-        }
-    }, 1000);     
-};
-
 function getQuestion() {
     var question = questions[currentQuestion];
-    document.getElementById('question').textContent = question.question;
+    document.getElementById("question").textContent = question.question;
     answer.innerHTML = "";
     for (var i = 0; i < question.choices.length; i++) {
         var newItem = document.createElement("li");
@@ -66,30 +50,46 @@ function getQuestion() {
     }
 };
 
+function timer() {
+    var timeLeft = 75;
+
+    var interval = setInterval(function() {
+        if (timeLeft > 0) {
+            timerClock.textContent = "Time Left: " + timeLeft;
+            timeLeft--; 
+        } else {
+            clearInterval(interval);
+            // show highscores.html - run function 
+        }
+    }, 1000);     
+};
+
 function processClick() {
     // say if question is right or wrong
     var question = questions[currentQuestion]
-    console.log(this);
+    //console.log(this);
     if (question.answer === this.textContent) {
-        // increase score
+        scoreCounter++;
     } else {
-        // deduct 10 seconds from timer and deduct 1 from score
-    }
+        timerClock - 10;
+    }    
 
-
-    if (currentQuestion) {
-
-    }
-    // load next question
-    currentQuestion++
-    getQuestion();
+    // deduct 10 seconds from timer and deduct 1 from score
+};
     
-}
-
+    currentQuestion++
+    if (currentQuestion === questions.length) {
+        // show highscore.html
+        // set quiz-wrapper to empty string, 
+        // create new variable 
+    } 
+    getQuestion();  
 
 
 
 getQuestion();
+
+// event listener to start quiz when user clicks on start button
 
 // when user clicks on start button, the timer starts
 startBtn.addEventListener("click", timer);

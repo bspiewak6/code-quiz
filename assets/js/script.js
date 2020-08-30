c05e138be1a64262b64fb0fd84e5000f431050cb
Spiewak <bspiewak6@gmail.com>
2020-08-30 12:38:11 -0400

    code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,9 @@ var result = document.getElementById("result");
 var currentQuestion = 0;
 var scoreCounter = 75;
 var timeLeft = 75;
+var highScore = 0
+var currentRoundScore = 0
+var interval
 
 
 
@@ -27,7 +30,7 @@ var questions = [
     {
     question: "Commonly used data types DO NOT Include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "booleans"
+    answer: "alerts"
     },   
     
     {
@@ -47,18 +50,19 @@ var questions = [
 
 function getQuestion() {
     if(questions[currentQuestion]){
-    var question = questions[currentQuestion];
-    document.getElementById("question").textContent = question.question;
-    answer.innerHTML = "";
-    for (var i = 0; i < question.choices.length; i++) {
-        var newItem = document.createElement("li");
-        newItem.textContent = question.choices[i];
-        answer.appendChild(newItem);
-        newItem.addEventListener("click", processClick)
+        var question = questions[currentQuestion];
+        document.getElementById("question").textContent = question.question;
+        answer.innerHTML = "";
+
+        for (var i = 0; i < question.choices.length; i++) {
+            var newItem = document.createElement("li");
+            newItem.textContent = question.choices[i];
+            answer.appendChild(newItem);
+            newItem.addEventListener("click", processClick)
+        }
     }
-}
 };
-var interval
+
 function timer() {
      interval = setInterval(function() {
         if (timeLeft > 0) {
@@ -75,24 +79,50 @@ function processClick() {
     // say if question is right or wrong
     result.innerHTML = "";
     var question = questions[currentQuestion];
+    var lastQuestion = questions[4]
+    console.log(lastQuestion);
     if (questions[currentQuestion]){
-        correctAlert = document.createElement("p");
-
+        var response = document.createElement("p");
+    }
     if (question.answer === this.textContent) {
-        correctAlert.textContent = "CORRECT";
-        
+        response.textContent = "CORRECT";
+    if (lastQuestion) {
+        clearInterval(interval);
+        console.log("whatever");
+    }
     } else {
-        correctAlert.textContent = "INCORRECT";
+        response.textContent = "INCORRECT";
         timeLeft = timeLeft - 10;
     }
-    result.appendChild(correctAlert);
+    result.appendChild(response);
     currentQuestion++;
     
     getQuestion();  
-}   else clearInterval(interval)
+
+// }   else 
+// clearInterval(interval);
+
+// get question from the question array if it exists
+// if it does exist, we want to create message box (p tag)
+// compare answers 
+// if it's correct, then the response "CORRECT" will show up at the bottom
+// if it is not correct, then the response "INCORRECT" will show up
+// if the answer is incorrect, the timer will subtract 10 seconds
+
 };
 
+// need to replace the original text on the page with the quiz q&a when start button is clicked
 
+// need to stop the timer after clicking last answer
+
+// need to use localStorage to save score which is the time left
+
+// take user to highscore.html for them to write in initials
+
+// save all high scores in localStorage to show on highscore.html page
+if (currentRoundScore > localStorage.getItem("highScore")) {
+    localStorage.setItem("highScore, currentRoundScore");
+}
 
 
 // event listener to start quiz when user clicks on start button
